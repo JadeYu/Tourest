1d5aa54bb7b8f9e0f448df987a33c268505d3e96
Jade <yzh828@berkeley.edu>
2017-02-13 17:14:37 -0800

    refactoring based on input flow

--- input_support.py
+++ input_support.py
@@ -1,4 +1,12 @@
+#Functions that convert input to appropriate data types or transform outcomes
+#for better visualization.
+
 def str2nlist(string):
+    """
+    Convert a string like '1,2,15' to a list like [1,2,15].
+    
+    Return the list.
+    """
     i = 0
     result = []
     sn = ''
@@ -13,6 +21,13 @@ def str2nlist(string):
     return result
 
 def add_num(names):
+    """
+    Add a number in front of each string element of the list.
+
+    For example: ["Betty", "Jimmy"] will become ["1. Betty", "2. Jimmy"].
+
+    Return the modified list.
+    """
     nnames = []
     for i in range(len(names)):
         nnames.append('{}. {}'.format(str(i+1), names[i]))
